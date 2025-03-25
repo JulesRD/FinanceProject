30d942b69ce10278b5896b0dd067b11ca1bf500d
dipezed <dcalmand73@gmail.com>
2025-03-25 13:56:37 +0100

    feat: for maxoupishou

--- etl/create_db.py
+++ etl/create_db.py
@@ -3,8 +3,15 @@ import numpy as np
 
 def create_db(df_bourso:pd.DataFrame, df_eronext:pd.DataFrame, db):
     print("TODO: create database")
+
+    return None
+
+def populate_companies(df_bourso:pd.DataFrame, df_eronext:pd.DataFrame, db):
+    print("TODO: populate companies")
+
     df_companies = df_bourso.copy()
     # fill with nan
+    
     df_companies["isin"] = np.nan
     df_companies["boursorama"] = df_companies["symbol"]
     df_companies["symbol"].apply(lambda x: x[3: len(x)])
@@ -17,4 +24,17 @@ def create_db(df_bourso:pd.DataFrame, df_eronext:pd.DataFrame, db):
     df_companies["sector3"] = np.nan # TODO
 
     db.df_write(df_companies, "companies")
+
+    return None
+
+def populate_markets(df_bourso:pd.DataFrame, df_eronext:pd.DataFrame, db):
+
+    return None
+
+def populate_stocks(df_bourso:pd.DataFrame, df_eronext:pd.DataFrame, db):
+
+    return None
+
+def populate_daystocks(df_bourso:pd.DataFrame, df_eronext:pd.DataFrame, db):
+
     return None
