7d254d98672a905d16b96eb9c78ce9d10f3f379e
jules.raitiere-delsupexhe <jules.raitiere-delsupexhe@epita.fr>
2025-03-31 11:44:00 +0200

    FIX: using the right databases and candlestick works

--- dashboard/app.py
+++ dashboard/app.py
@@ -10,43 +10,61 @@ import dash_bootstrap_components as dbc
 from mylogging import getLogger
 import timescaledb_model as tsdb
 
-
-
 mylogger = getLogger(__name__)
 
 db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp', remove_all=True)
 
 ## Boursorama
 
-
-
-
 ### MOCKING data
 date_range = pd.date_range(start='2025-03-01', end='2025-04-01', freq='D')
 value = np.random.uniform(low=10, high=100, size=100)
 data = {
     'date': np.random.choice(date_range, size=100),
-    'action': np.random.choice(['Action 1', 'Action 2', 'Action 3', 'Action 4', 'Action 5', 'Action 6', 'Action 7', 'Action 8', 'Action 9', 'Action 10', 'Action 11', 'Action 12'], size=100),
+    'cid': np.random.randint(1, 13, size=100),  # Assuming 12 companies
     'value': np.random.uniform(low=10, high=100, size=100),
-
-    # Ajouter des colonnes pour les valeurs open, high, low, close
-    'open': np.random.uniform(low=10, high=100, size=100),
-    'close': np.random.uniform(low=10, high=100, size=100),
+    'volume': np.random.uniform(low=100, high=1000, size=100),
 }
 
-data['high'] = data['value'] + np.random.uniform(low=0, high=10, size=100)
-data['low'] = data['value'] - np.random.uniform(low=0, high=10, size=100)
+daystocks_data = {
+    'date': np.random.choice(date_range, size=100),
+    'cid': np.random.randint(1, 13, size=100),
+    'open': np.random.uniform(low=10, high=100, size=100),
+    'close': np.random.uniform(low=10, high=100, size=100),
+    'high': np.random.uniform(low=10, high=110, size=100),
+    'low': np.random.uniform(low=0, high=90, size=100),
+    'volume': np.random.uniform(low=100, high=1000, size=100),
+    'mean': np.random.uniform(low=10, high=100, size=100),
+    'std': np.random.uniform(low=1, high=10, size=100),
+}
 
-print("\n\n\nEND_OF_DATA\n\n\n")
-df = pd.DataFrame(data)
-db.df_write(df, 'stock_data')
-###
+companies_data = {
+    'id': np.arange(1, 13),
+    'name': [f'Action {i}' for i in range(1, 13)],
+    'mid': np.random.randint(1, 8, size=12),  # Assuming 7 markets
+    'symbol': [f'SYM{i}' for i in range(1, 13)],
+    'isin': [f'IS{i:010}' for i in range(1, 13)],
+    'boursorama': [f'BOUR{i}' for i in range(1, 13)],
+    'euronext': [f'EUR{i}' for i in range(1, 13)],
+    'pea': np.random.choice([True, False], size=12),
+    'sector1': [f'Sector1_{i}' for i in range(1, 13)],
+    'sector2': [f'Sector2_{i}' for i in range(1, 13)],
+    'sector3': [f'Sector3_{i}' for i in range(1, 13)],
+}
+
+df_stocks = pd.DataFrame(data)
+df_daystocks = pd.DataFrame(daystocks_data)
+df_companies = pd.DataFrame(companies_data)
+
+db.df_write(df_companies, 'companies')
+db.df_write(df_stocks, 'stocks')
+db.df_write(df_daystocks, 'daystocks')
 
 external_stylesheets=[dbc.themes.BOOTSTRAP]
 app = dash.Dash(__name__,  title="Bourse", suppress_callback_exceptions=True,
                 external_stylesheets=external_stylesheets, assets_ignore='style.css?v=1.0')
-app.df = df
-app.daydf = pd.DataFrame()
+app.df = df_stocks
+app.daydf = df_daystocks
 app.comp_names = []
 server = app.server
 
@@ -54,5 +72,4 @@ from index import layout  # Not before app is defined since we use it
 app.layout = layout
 
 if __name__ == '__main__':
-
     app.run(debug=True)
--- dashboard/tabs/tab1.py
+++ dashboard/tabs/tab1.py
@@ -22,48 +22,56 @@ def update_graph(start_date, end_date, selected_actions, display_mode):
     if not start_date or not end_date or not selected_actions:
         return {}
 
-    # Query the database for the selected actions and date range
-    query = f"""
-    SELECT date, action, value, open, low, high, close, 
-    FROM stock_data
-    WHERE action IN ({', '.join([f"'{action}'" for action in selected_actions])})
-    AND date BETWEEN '{start_date}' AND '{end_date}'
-    ORDER BY date
-    """
+    if display_mode:
+        # Query the daystocks table for candlestick mode
+        query = f"""
+        SELECT date, cid, open, low, high, close
+        FROM daystocks
+        WHERE cid IN ({', '.join([f"{cid}" for cid in selected_actions])})
+        AND date BETWEEN '{start_date}' AND '{end_date}'
+        ORDER BY date
+        """
+    else:
+        # Query the stocks table for line mode
+        query = f"""
+        SELECT date, cid, value
+        FROM stocks
+        WHERE cid IN ({', '.join([f"{cid}" for cid in selected_actions])})
+        AND date BETWEEN '{start_date}' AND '{end_date}'
+        ORDER BY date
+        """
 
     mylogger.debug(f"\nQUERY: {query}\n")
     df = db.df_query(query)
     mylogger.debug(f"\ndf: {df}\n")
-    #mylogger.debug(f"\n\n\nDF: {df}\n\n\n")
+
     # Create the graph
     if display_mode:
         fig = go.Figure(data=[go.Candlestick(x=df['date'],
-                                            open=df['open'],
-                                            high=df['high'],
-                                            low=df['low'],
-                                            close=df['close'])])
+                                             open=df['open'],
+                                             high=df['high'],
+                                             low=df['low'],
+                                             close=df['close'])])
         fig.update_layout(title='Stock Values Over Time (Candlestick)')
     else:
-        fig = px.line(df, x='date', y='value', color='action', title='Stock Values Over Time')
-    # save de figure
+        fig = px.line(df, x='date', y='value', color='cid', title='Stock Values Over Time')
+
+    # Save the figure
     fig.write_image("./fig1.png")
     return fig
 
-
 get_actions_query = f"""
-SELECT DISTINCT action
-FROM stock_data
-ORDER BY action
+SELECT id, name
+FROM companies
+ORDER BY name
 """
 list_actions = db.df_query(get_actions_query)
-list_actions = list(list_actions["action"])
-
-print(list_actions)
+list_actions = list(list_actions.itertuples(index=False, name=None))
 
 tab1_layout = html.Div([
     html.Div([
         html.H1("J'ai un changement"),
-        
+
         # Sélecteur de période
         html.Div([
             html.Label("Choisissez la période:"),
@@ -74,15 +82,15 @@ tab1_layout = html.Div([
                 calendar_orientation='vertical'
             )
         ], className="date-picker-container"),
-        
+
         # Sélecteur d'actions (multi-sélection)
         html.Div([
             html.Label("Choisissez vos actions parmis les suivantes:"),
             dcc.Dropdown(
                 id="actions-dropdown",
                 options=[
-                    {'label': action, 'value': action} for action in list_actions
-                    ],
+                    {'label': name, 'value': cid} for cid, name in list_actions
+                ],
                 multi=True,
                 placeholder="Sélectionnez jusqu'à 2 actions"
             )
@@ -91,9 +99,11 @@ tab1_layout = html.Div([
         # Sélecteur de mode d'affichage
         html.Div([
             html.Label("Mode d'affichage:"),
-            dcc.Checkbox(
+            dbc.Checklist(  # Use Checklist from dash_bootstrap_components
                 id='display-mode-checkbox',
-                value=False  # Default to line mode
+                options=[{'label': 'Candlestick', 'value': True}],
+                value=[],  # Default to line mode
+                switch=True,
             )
         ], className="display-mode-container"),
 
